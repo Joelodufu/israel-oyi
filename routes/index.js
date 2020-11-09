457157fdf42405df09348007c91fe96e1ef37d84
joel odufu <joel.odufu@gmail.com>
2020-11-09 22:44:47 +0100

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+const indexcontroller = require('../controllers/indexcontroller');
+
+/* GET home page. */
+router.get('/', indexcontroller.index);
+
+module.exports = router;
